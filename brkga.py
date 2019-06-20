de950b7831a1a896ea3403cc16c6de2450186ede
Peter Caven <peter@sparseinference.com>
2019-06-20 03:23:21 -0400

    add link to reference

--- brkga.py
+++ brkga.py
@@ -1,16 +1,13 @@
 """
 File:  brkga.py
-Created: 2019-05-09
-by Peter Caven, peter@sparseinference.com
+Created: 2019-06-16
+Copyright 2019 Peter Caven, peter@sparseinference.com
 
 Description:
 
 An implementation of the "Biased Random-Key Genetic Algorithm".
-[1] Random-key genetic algorithms, by José Fernando Gonçalves, Mauricio G. C. Resende, August 18, 2014
-
-
-
-
+[1] Random-key genetic algorithms, by José Fernando Gonçalves, Mauricio G. C. Resende, 
+    August 18, 2014, http://mauricio.resende.info/
 
 
 """
@@ -102,21 +99,6 @@ class BRKGA(torch.nn.Module):
 ##===================================================
 
 if __name__ == '__main__':
-    # --- init and print the initial keys ---
-    popCount = 10
-    pop = BRKGA((popCount,2,2), eliteFraction=0.2, mutantFraction=0.1)
-    print("pop before sorting:\n", pop.keys)
-    # --- 
-    def cost(keys):
-        return torch.rand(len(keys))
-    # --- 
-    results = cost(pop.keys)
-    print("results:\n", results)
-    pop.orderBy(results)
-    print("pop after sorting:\n", pop.keys)
-    # --- One BRKGA iteration ---
-    pop.evolve()
-    print("pop after evolve:\n", pop.keys)
-    #pop.zero_grad() # if needed
-    print("elites:\n", pop.elites)
+    pass
 
+    
